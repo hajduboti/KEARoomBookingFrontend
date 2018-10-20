d5191e1ac77ba5940324eba3053bd479e9f93a3f
unknown <boto0018@stud.kea.dk>
2018-10-20 16:00:42 +0200

    register added as well as routing

--- RoomBooker/src/app/app.module.ts
+++ RoomBooker/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+
+
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -29,6 +32,7 @@ import { LoginComponent } from './login/login.component';
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatButtonModule,
     MatMenuModule,
     AppRoutingModule,
     FormsModule,
