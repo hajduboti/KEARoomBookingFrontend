6295242378d150b00d307899ca7255613bcef655
Gabriele Mannucci <gabr2164@stud.kea.dk>
2018-11-27 13:18:35 +0100

    quick fix

--- RoomBooker/src/app/navbar/navbar.component.ts
+++ RoomBooker/src/app/navbar/navbar.component.ts
@@ -10,22 +10,22 @@ export class NavbarComponent implements OnInit {
   show;
   constructor(private as:AuthService) { }
   ngOnInit() {
-    let tokenKey = JSON.parse(localStorage.getItem('currentUser'));
-
-    try{
-        let token = tokenKey['key'];
-      }catch(e){
-        let token = undefined;
-      }
-      if(token !== undefined){
-      console.log('not showing');
-      console.log(token);
-      show = false;
-
-    }else{
-      console.log('showing');
-      show = true;
-    }
+    // let tokenKey = JSON.parse(localStorage.getItem('currentUser'));
+    //
+    // try{
+    //     let token = tokenKey['key'];
+    //   }catch(e){
+    //     let token = undefined;
+    //   }
+    //   if(token !== undefined){
+    //   console.log('not showing');
+    //   console.log(token);
+    //   show = false;
+    //
+    // }else{
+    //   console.log('showing');
+    //   show = true;
+    // }
   }
 
 
