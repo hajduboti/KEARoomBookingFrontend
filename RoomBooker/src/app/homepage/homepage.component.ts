1186603cc8765ab2ef76978820d6cede013800f2
Gabriele Mannucci <gabr2164@stud.kea.dk>
2018-11-27 13:15:31 +0100

    logout trial

--- RoomBooker/src/app/homepage/homepage.component.ts
+++ RoomBooker/src/app/homepage/homepage.component.ts
@@ -39,7 +39,8 @@ export class HomepageComponent implements OnInit {
     console.log(this.bs.getRooms("startDate=" + workingDate + " " + fromtime + "&endDate=" + workingDate + " " + totime));
   }
 
-
-
+  logOut(){
+    localStorage.clear();
+  }
 
 }
--- RoomBooker/src/app/navbar/navbar.component.ts
+++ RoomBooker/src/app/navbar/navbar.component.ts
@@ -7,10 +7,26 @@ import { AuthService } from '../auth/auth.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  show;
   constructor(private as:AuthService) { }
   ngOnInit() {
+    let tokenKey = JSON.parse(localStorage.getItem('currentUser'));
+
+    try{
+        let token = tokenKey['key'];
+      }catch(e){
+        let token = undefined;
+      }
+      if(token !== undefined){
+      console.log('not showing');
+      console.log(token);
+      show = false;
+
+    }else{
+      console.log('showing');
+      show = true;
+    }
   }
 
-  
 
 }
